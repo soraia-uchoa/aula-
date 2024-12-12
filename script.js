cd05d00bad0c2ee1f6cae87a6f636b73b91cd1ac
soraia-uchoa <soraiavercosa3@icloud.com>
2024-12-12 08:53:10 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Olá mundo!</title>
+    <p>lorem ipsum dolor sit emet.</p>
+    <button>Clique aqui</button>
+</head>
+<body>
+   
+    <ol>
+<li>soraia</li>
+<li>arroz</li>
+    </ol>
+
+  ABRIR A CAIXINHA DE PERGUNTAS
+   <ul> 
+    <li>bem-vindo</li>
+    <li>escreva seu nome</li>
+   </ul>
+
+<input type="text">
+
+    PODE TROCAR POR = PASSWORD
+    
